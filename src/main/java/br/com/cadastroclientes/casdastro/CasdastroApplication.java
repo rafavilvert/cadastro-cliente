11b88a0481ce8265e709e5bf650bf0f0992ec146
Rafael Vilvert <vilvert@pop-os.localdomain>
2021-12-02 20:09:52 -0300

    First commit

---
+++ src/main/java/br/com/cadastroclientes/casdastro/CasdastroApplication.java
@@ -0,0 +1,13 @@
+package br.com.cadastroclientes.casdastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasdastroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasdastroApplication.class, args);
+	}
+
+}
